9436984ffd6207e1a54a9db16345240ce7e96540
Nexis <NexisNexis@protonmail.com>
2026-05-02 13:47:10 +0200

    fixed URL routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Loader2 } from 'lucide-react';
 
 import { useNostr } from './hooks/useNostr';
